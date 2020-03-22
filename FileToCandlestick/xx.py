279b55b85a23f04de48f5b7b8912365fcbe83545
Julian <moshammer.ju@gmail.com>
2020-03-22 10:56:40 +0100

    final

--- FileToCandlestick/xx.py
+++
@@ -1,5 +0,0 @@
-file = pd.read_csv('1M_nasdaq_20191201-20191231_1m.csv', infer_datetime_format=True, delimiter=r";")
-file['Date'] = pd.to_datetime(file['Date'])
-file.set_index('Date', inplace=True)
-iday = file.loc['01/12/2019':'01/12/2019':]
-mpf.plot(iday, type='candle')
